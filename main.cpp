ab84defa997cfbb7c89eb8ec9c4c1a7cac33b279
Alexander <65162943+FirstAfterGod2501@users.noreply.github.com>
2020-10-04 20:35:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -58,3 +58,27 @@ std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "win! +"
         system("cls");
         }
 }
+/*
+do
+do class
+do class if
+do
+do class
+do class if
+
+do!
+do class!
+do class if
+do class if!
+do class if inline
+do class if inline
+do class if inline
+bool this delete define!
+
+this do int break sizeof public
+try if struct for auto static
+while!
+while!
+
+Rammstein
+*/
