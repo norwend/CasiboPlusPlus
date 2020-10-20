3588f9fea20bc9723ba44eca98d3649b7834af05
Stepan Ilyin <norwend@tuta.io>
2020-10-21 01:26:29 +0300

    changed the functions

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 #include <cstdlib> // для функций rand() и srand()
 using namespace std;
 
-int roll() {
+int random_num() 
+{
     int max = 7;
-    int first_num = (rand()%(max+1)+rand()%(max+1))/2;
-    int second_num = (rand()%(max+1)+rand()%(max+1))/2;
-    int third_num = (rand()%(max+1)+rand()%(max+1))/2;
+	return (rand()%(max+1)+rand()%(max+1))/2;
+}
+
+void print_nums (int first_num, int second_num, int third_num) 
+{
     cout << "| " << first_num << " | " << second_num << " | " << third_num << " |\n"; 
+}
+
+int roll() {
+    int first_num = random_num();
+    int second_num = random_num();
+    int third_num = random_num();
+	print_nums(first_num, second_num, third_num);
     if ((first_num == second_num) || (third_num == second_num) || (first_num == third_num)) return 1;
     if ((first_num == second_num) && (third_num == second_num)) return 2;
     return 0;
