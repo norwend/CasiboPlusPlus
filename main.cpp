7f75d3295c8b14c956cab27f93b9a86f04212ce9
Stepan Ilyin <norwend@tuta.io>
2020-10-21 01:27:15 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int random_num() 
 {
-    int max = 7;
+	int max = 7;
 	return (rand()%(max+1)+rand()%(max+1))/2;
 }
 
@@ -17,7 +17,7 @@ int roll() {
     int first_num = random_num();
     int second_num = random_num();
     int third_num = random_num();
-	print_nums(first_num, second_num, third_num);
+    print_nums(first_num, second_num, third_num);
     if ((first_num == second_num) || (third_num == second_num) || (first_num == third_num)) return 1;
     if ((first_num == second_num) && (third_num == second_num)) return 2;
     return 0;
