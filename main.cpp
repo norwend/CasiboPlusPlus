dea006134f6ebc604eb004dfc1951fea468cbb30
Stepan Ilyin <norwend@tuta.io>
2020-10-20 20:57:49 +0300

    major code change
    cleaned up, refactored, rewrote functions

--- main.cpp
+++ main.cpp
@@ -1,63 +1,86 @@
 #include <iostream>
-#include <ctime>
+//#include <ctime>
 #include <cstdlib> // для функций rand() и srand()
-#include <windowsx.h>
-unsigned short getRandomNumber()
-{
-    int min = 1;
+//#include <windowsx.h>
+//int getRandomNumber();
+using namespace std;
+
+int roll() {
     int max = 7;
-    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-    // Равномерно распределяем рандомное число в нашем диапазоне
-    return static_cast<unsigned short>(rand() * fraction * (max - min + 1) + min);
+    int first_num = rand()%(max+1);
+    int second_num = rand()%(max+1);
+    int third_num = rand()%(max+1);
+    cout << '| ' << first_num << " | " << second_num << " | " << third_num << " |\n"; 
+    if ((first_num == second_num) || (third_num == second_num) || (first_num == third_num)) return 1;
+    if ((first_num == second_num) && (third_num == second_num)) return 2;
+    return 0;
 }
 
 int main()
 {
-        setlocale(LC_ALL,"Russian");
-    unsigned short FisrtNum = 0;
-    unsigned short SecondNum = 0;
-    unsigned short ThirdNum = 0;
-    long long MySchore = 2000;
-    while (true) {
-        std::cout << "Введите ставку" << std::endl;
-        std::cout <<"Ваш балланс "<< MySchore << "\n";
-        int stavka = 0;
-        std::cin >> stavka;
-        if (stavka > MySchore) {
-            std::cout << "idi naxuy\n";
-        }
-        else {
-            FisrtNum = getRandomNumber();
-            SecondNum = getRandomNumber();
-            ThirdNum = getRandomNumber();
-            if (FisrtNum == SecondNum == ThirdNum) {
-std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "777 win! +" << stavka * 10 << "\n";
-MySchore += stavka * 10;
-                }
-            else if ((FisrtNum == SecondNum) || (FisrtNum == ThirdNum)) {
-std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "win! +" << stavka * 5 << "\n";
-                MySchore += stavka * 5;
+    //setlocale(LC_ALL,"Russian"); -- для виндопедиков
+    
+    long long score = 2000;
+    try {
+        while (true) {
+            cout <<"Ваш балланс: "<< score << "\n";
+            cout << "Введите ставку" << endl;
+            int bet = 0;
+            cin >> bet;
+        
+            if (bet > score) {
+              cout << "Ставка слишком велика! (пошёл нахуй)\n";
+              break;
             }
-            else if (SecondNum == ThirdNum) {
-std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "win! +" << stavka * 5 << "\n";
-                MySchore += stavka * 5;
+            switch (roll()) {
+                case 0:
+                    cout << "проебал!\t-" << bet << '\n';
+                    score -= bet;
+                    break;
+                case 1:
+                    cout << "win!\t+" << bet * 5 << '\n';
+                    score += bet * 5;
+                    break;
+                case 2:
+                    cout << "big win!\t+" << bet * 100 << '\n';
+                    score += bet * 100;
+                    break;
+                default:
+                    throw runtime_error("suka ty kak eto slomal");
             }
-            else {
-            std::cout<<FisrtNum << " " << SecondNum << " " << ThirdNum << "\n"<<"неть -"<<stavka<<"\n";
-            MySchore -= stavka;
-                }
-        }
-         int a = 0;
-        //std::cin>>a;
-        if(MySchore == 0){
-                MySchore += 2000;
+            //if (FisrtNum == SecondNum == ThirdNum) {
+            //        std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "big win! \t+" << bet * 10 << "\n";
+            //        MyScore += bet * 10;
+            //}
+            //else if ((FisrtNum == SecondNum) || (FisrtNum == ThirdNum) || (SecondNum == ThirdNum)) {
+            //    std::cout << FisrtNum << " " << SecondNum << " " << ThirdNum << "\n" << "win! \t+" << bet * 5 << "\n";
+            //    MyScore += bet * 5;
+            //}
+            //else {
+            //    std::cout<<FisrtNum << " " << SecondNum << " " << ThirdNum << "\n"<<"lose :(\t -"<< bet << "\n";
+            //    MyScore -= bet;
+            //
+            if(score == 0){
+                score += 2000;
                 a++;
-                std::cout<<a;
+                cout << "lose count: " << a;
             }
-        system("Pause");
-        system("cls");
         }
+    }
+    catch (exception& e) {
+        cerr << e.what();
+    }
 }
+
+//int getRandomNumber()
+//{
+    //int max = 7;
+    //static const double fraction = 1.0 / (narrow_cast<double>(RAND_MAX) + 1.0);
+    // Равномерно распределяем рандомное число в нашем диапазоне
+    //return narrow_cast<unsigned short>(rand() * fraction * max + min);
+    //return rand()%(max+1);
+//}
+
 /*
 do
 do class
