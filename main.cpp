67d58a113325d02cc75319dd00703810f16a1d1b
Stepan Ilyin <norwend@tuta.io>
2020-12-02 16:34:06 +0300

    a

--- main.cpp
+++ main.cpp
@@ -12,28 +12,3 @@ int main()
         std::cerr << e.what();
     }
 }
-
-/*
-do
-do class
-do class if
-do
-do class
-do class if
-
-do!
-do class!
-do class if
-do class if!
-do class if inline
-do class if inline
-do class if inline
-bool this delete define!
-
-this do int break sizeof public
-try if struct for auto static
-while!
-while!
-
-Rammstein
-*/
